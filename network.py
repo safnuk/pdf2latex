ab6962aaa1a0fb35d7d38db9843c93378782f335
Brad Safnuk <brad.safnuk@cmich.edu>
2017-04-12 01:38:18 -0400

    Clean up network code

--- network.py
+++ network.py
@@ -1,5 +1,4 @@
 import collections
-import re
 
 import tensorflow as tf
 
@@ -17,6 +16,7 @@ class Network:
         test_size,
         learning_rate=0.1,
         learning_rate_decay_factor=.1,
+        num_steps=1000,
         rnn_cell_size=10,
         num_rnn_layers=1,
         conv_filter_sizes=FilterSizes(5, 3, 5),
@@ -24,40 +24,79 @@ class Network:
             **{'chars': 3, 'fonts': 2, 'fontsizes': 1, 'tokens': 10}),
         use_lstm=False,
         data_dir='data/',
-        use_fp16=False,
-        tower_name='tower',
+        log_dir='log/',
     ):
         self.batch_size = batch_size
         self.rnn_cell_size = rnn_cell_size
         self.num_rnn_layers = num_rnn_layers
         self.filters = conv_filter_sizes
         self.data_dir = data_dir
+        self.log_dir = log_dir
         self.embedding_dims = embedding_dims
         self.input_dim = (embedding_dims.chars + embedding_dims.fonts +
                           embedding_dims.fontsizes)
         self.use_lstm = use_lstm
-        self.dtype = tf.float16 if use_fp16 else tf.float32
+        self.validation_size = validation_size
+        self.test_size = test_size
         self.data = dataset.read_datasets('data/', validation_size, test_size)
-        self.tower_name = tower_name
         self.learning_rate = tf.Variable(
-            float(learning_rate), trainable=False, dtype=self.dtype)
+            float(learning_rate), trainable=False)
         self.learning_rate_decay_op = self.learning_rate.assign(
             self.learning_rate * learning_rate_decay_factor)
-        self.global_step = tf.Variable(0, trainable=False)
+        self.global_step = tf.Variable(0, name='global_step', trainable=False)
+        self.num_steps = num_steps
 
     def build(self):
         # Feeds for inputs.
-        self.input_pdfs = tf.placeholder(tf.int32, shape=[None, 366, 100, 3],
-                                         name='input')
+        self.feature_input = tf.placeholder(tf.int32, shape=[None, 366, 100, 3])
         self.tokens = tf.placeholder(
             tf.int32, shape=[None, self.data.token_sequence_length],
-            name='tokens'
         )
-        e = self.embedding_layers(self.input_pdfs)
+        e = self.embedding_layers(self.feature_input)
         c = self.convolution_layers(e)
         r = self.seq2seq_layer(c, self.tokens)
-        loss = self.loss_layer(r, self.tokens)
-        self.rnn = self.train_layer(loss)
+        self.loss = self.loss_layer(r, self.tokens)
+        self.rnn = self.train_layer(self.loss)
+
+    def feed_dict(self, train=True):
+        if train:
+            feature_batch, token_batch = self.data.train.next_batch(
+                self.batch_size)
+        else:
+            feature_batch, token_batch = self.data.validate.next_batch(
+                self.validation_size)
+        return {self.feature_input: feature_batch, self.tokens: token_batch}
+
+    def train(self):
+        sess = tf.InteractiveSession()
+        merged = tf.summary.merge_all()
+        writer = tf.summary.FileWriter(self.log_dir + '/train', sess.graph)
+        test_writer = tf.summary.FileWriter(self.log_dir + '/test')
+        tf.global_variables_initializer().run()
+        for n in range(self.num_steps):
+            if n % 10 == 0:
+                summary, acc = sess.run([merged, self.accuracy],
+                                        feed_dict=self.feed_dict(False))
+                test_writer.add_summary(summary, n)
+                print('Accuracy at step %s: %s' % (n, acc))
+            else:
+                if n % 100 == 99:  # Record execution stats
+                    run_options = tf.RunOptions(
+                        trace_level=tf.RunOptions.FULL_TRACE)
+                    run_metadata = tf.RunMetadata()
+                    summary, _ = sess.run([merged, self.rnn],
+                                          feed_dict=self.feed_dict(True),
+                                          options=run_options,
+                                          run_metadata=run_metadata)
+                    writer.add_run_metadata(run_metadata, 'step%03d' % n)
+                    writer.add_summary(summary, n)
+                    print('Adding run metadata for', n)
+                else:  # Record a summary
+                    summary, _ = sess.run([merged, self.rnn],
+                                          feed_dict=self.feed_dict(True))
+                    writer.add_summary(summary, n)
+            writer.close()
+            test_writer.close()
 
     def _activation_summary(self, x):
         """Helper to create summaries for activations.
@@ -68,48 +107,22 @@ class Network:
         Returns:
             nothing
         """
-        # Remove 'tower_[0-9]/' from the name in case this is a multi-GPU
-        # training session. This helps the clarity on tensorboard.
-        tensor_name = re.sub('%s_[0-9]*/' % self.tower_name, '', x.op.name)
-        tf.summary.histogram(tensor_name + '/activations', x)
-        tf.summary.scalar(tensor_name + '/sparsity', tf.nn.zero_fraction(x))
+        with tf.name_scope('summaries'):
+            tensor_name = x.op.name
+            tf.summary.histogram(tensor_name + '/activations', x)
+            tf.summary.scalar(tensor_name + '/sparsity', tf.nn.zero_fraction(x))
 
-    def _variable_on_cpu(self, name, shape, initializer):
-        """Helper to create a Variable stored on CPU memory.
-        Args:
-            name: name of the variable
-            shape: list of ints
-            initializer: initializer for Variable
-        Returns:
-            Variable Tensor
-        """
-        with tf.device('/cpu:0'):
-            var = tf.get_variable(name, shape,
-                                  initializer=initializer, dtype=self.dtype)
-        return var
+    def _embedding_variable(self, vocab_size, embedding_dim):
+        initial = tf.random_uniform([vocab_size, embedding_dim], -1.0, 1.0)
+        return tf.Variable(initial, name='embedding')
 
-    def _variable_with_weight_decay(self, name, shape, stddev, wd):
-        """Helper to create an initialized Variable with weight decay.
-        Note that the Variable is initialized with a truncated normal
-        distribution. A weight decay is added only if one is specified.
-        Args:
-            name: name of the variable
-            shape: list of ints
-            stddev: standard deviation of a truncated Gaussian
-            wd: add L2Loss weight decay multiplied by this float.
-                 If None, weight decay is not added for this Variable.
-        Returns:
-            Variable Tensor
-        """
-        var = self._variable_on_cpu(
-            name,
-            shape,
-            tf.truncated_normal_initializer(stddev=stddev, dtype=self.dtype))
-        if wd is not None:
-            weight_decay = tf.multiply(tf.nn.l2_loss(var),
-                                       wd, name='weight_loss')
-            tf.add_to_collection('losses', weight_decay)
-        return var
+    def _kernel_variable(self, shape):
+        initial = tf.truncated_normal(shape, stddev=5e-2)
+        return tf.Variable(initial, name='kernel')
+
+    def _bias_variable(self, shape, constant=0.0):
+        initial = tf.constant(constant, shape=shape)
+        return tf.Variable(initial, name='bias')
 
     def embedding_layers(self, input_pdfs):
         """Embed the input data into dense vector representations.
@@ -119,35 +132,30 @@ class Network:
         Returns:
             Dense vector space representation of the input data.
         """
-        slices = tf.unstack(input_pdfs, axis=3)
         vocab_size = self.data.feature_vocab_size
-        with tf.variable_scope('char_embed') as scope:
-            embeddings = self._variable_on_cpu(
-                'embeddings',
-                [vocab_size.chars, self.embedding_dims.chars],
-                tf.random_uniform_initializer(-1.0, 1.0)
-            )
-            input_embed = tf.nn.embedding_lookup(embeddings,
-                                                 slices[0], name=scope.name)
-        with tf.variable_scope('font_embed') as scope:
-            embeddings = self._variable_on_cpu(
-                'embeddings',
-                [vocab_size.fonts, self.embedding_dims.fonts],
-                tf.random_uniform_initializer(-1.0, 1.0)
-            )
-            font_embed = tf.nn.embedding_lookup(embeddings,
-                                                slices[1], name=scope.name)
-        with tf.variable_scope('fontsize_embed') as scope:
-            embeddings = self._variable_on_cpu(
-                'embeddings',
-                [vocab_size.fontsizes, self.embedding_dims.fontsizes],
-                tf.random_uniform_initializer(-1.0, 1.0)
-            )
-            fontsize_embed = tf.nn.embedding_lookup(embeddings,
-                                                    slices[2], name=scope.name)
-
-        embedded = tf.concat([input_embed, font_embed, fontsize_embed], 3)
-        return embedded
+        with tf.name_scope("slice_features"):
+            slices = tf.unstack(input_pdfs, axis=3)
+        with tf.name_scope('char_embed'):
+            embeddings = self._embedding_variable(
+                vocab_size.chars, self.embedding_dims.chars)
+            input_embed = tf.nn.embedding_lookup(embeddings, slices[0])
+            self._activation_summary(embeddings)
+        with tf.variable_scope('font_embed'):
+            embeddings = self._embedding_variable(
+                vocab_size.fonts, self.embedding_dims.fonts)
+            font_embed = tf.nn.embedding_lookup(
+                embeddings, slices[1])
+            self._activation_summary(embeddings)
+        with tf.name_scope('fontsize_embed'):
+            embeddings = self._embedding_variable(
+                vocab_size.fontsizes, self.embedding_dims.fontsizes)
+            fontsize_embed = tf.nn.embedding_lookup(
+                embeddings, slices[2])
+            self._activation_summary(embeddings)
+        with tf.name_scope('feature_embed'):
+            embedded = tf.concat([input_embed, font_embed, fontsize_embed], 3)
+            self._activation_summary(embeddings)
+            return embedded
 
     def convolution_layers(self, embedded):
         """Build the convolution layers of the network.
@@ -157,66 +165,56 @@ class Network:
         Returns:
             Application of three convolution layers.
         """
-        # We instantiate all variables using tf.get_variable() instead of
-        # tf.Variable() in order to share variables across multiple GPUs.
-        # If we only ran this model on a single GPU, we could simplify this
-        # function by replacing all instances of tf.get_variable()
-        #  with tf.Variable().
-        with tf.variable_scope('conv1') as scope:
-            kernel = self._variable_with_weight_decay(
-                'weights', shape=[4, 6, self.input_dim, self.filters.conv1],
-                stddev=5e-2, wd=0.0)
+        with tf.name_scope('conv1'):
+            kernel = self._kernel_variable(
+                [4, 6, self.input_dim, self.filters.conv1])
             conv = tf.nn.conv2d(embedded, kernel, [1, 2, 2, 1], padding='VALID')
-            biases = self._variable_on_cpu(
-                'biases', [self.filters.conv1], tf.constant_initializer(0.0))
+            biases = self._bias_variable([self.filters.conv1], 0.1)
             pre_activation = tf.nn.bias_add(conv, biases)
-            conv1 = tf.nn.relu(pre_activation, name=scope.name)
+            conv1 = tf.nn.relu(pre_activation)
             self._activation_summary(conv1)
-        with tf.variable_scope('conv2') as scope:
-            kernel = self._variable_with_weight_decay(
-                'weights', shape=[3, 6, self.filters.conv1, self.filters.conv2],
-                stddev=5e-2, wd=0.0)
+        with tf.name_scope('conv2'):
+            kernel = self._kernel_variable(
+                [3, 6, self.filters.conv1, self.filters.conv2])
             conv = tf.nn.conv2d(conv1, kernel, [1, 3, 3, 1], padding='VALID')
-            biases = self._variable_on_cpu('biases', [self.filters.conv2],
-                                           tf.constant_initializer(0.1))
+            biases = self._bias_variable([self.filters.conv2], 0.1)
             pre_activation = tf.nn.bias_add(conv, biases)
-            conv2 = tf.nn.relu(pre_activation, name=scope.name)
+            conv2 = tf.nn.relu(pre_activation)
             self._activation_summary(conv2)
-        with tf.variable_scope('conv3') as scope:
-            kernel = self._variable_with_weight_decay(
-                'weights', shape=[3, 3, self.filters.conv2, self.filters.conv3],
-                stddev=5e-2, wd=0.0)
-            conv = tf.nn.conv2d(conv2, kernel,
-                                [1, 1, 2, 1], padding='SAME')
-            biases = self._variable_on_cpu(
-                'biases', [self.filters.conv3], tf.constant_initializer(0.1))
+        with tf.name_scope('conv3'):
+            kernel = self._kernel_variable(
+                [3, 3, self.filters.conv2, self.filters.conv3])
+            conv = tf.nn.conv2d(conv2, kernel, [1, 1, 2, 1], padding='SAME')
+            biases = self._bias_variable([self.filters.conv3], 0.1)
             pre_activation = tf.nn.bias_add(conv, biases)
-            conv3 = tf.nn.relu(pre_activation, name=scope.name)
+            conv3 = tf.nn.relu(pre_activation)
             self._activation_summary(conv3)
         return conv3
 
     def seq2seq_layer(self, conv, token_inputs):
-        rnn_inputs = tf.reshape(conv, [self.batch_size, -1, self.filters.conv3])
+        with tf.name_scope('flatten'):
+            conv_shape = tf.shape(conv)
+            rnn_inputs = tf.reshape(
+                conv, [conv_shape[0], -1, self.filters.conv3])
+        with tf.name_scope('reverse'):
+            rnn_reversed = tf.reverse(rnn_inputs, [-1])
         num_tokens = self.data.token_vocab_size
         with tf.variable_scope('encoder'):
             cell = self._rnn_cell()
-            _, encoded_state = tf.nn.dynamic_rnn(cell, rnn_inputs,
-                                                 dtype=self.dtype)
+            _, encoded_state = tf.nn.dynamic_rnn(
+                cell, rnn_reversed, dtype=tf.float32)
             self._activation_summary(encoded_state)
 
-        with tf.variable_scope('embed_tokens') as scope:
+        with tf.name_scope('prepend_GO'):
             token_shape = tf.shape(token_inputs)
             slice_shape = [token_shape[0], 1]
             go_array = tf.fill(slice_shape, self.data.GO_TOKEN)
             tokens_with_GO = tf.concat([go_array, token_inputs], 1)
-            embeddings = self._variable_on_cpu(
-                'embeddings',
-                [num_tokens, self.embedding_dims.tokens],
-                tf.random_uniform_initializer(-1.0, 1.0)
-            )
-            token_embedding = tf.nn.embedding_lookup(
-                embeddings, tokens_with_GO, name=scope.name)
-
+        with tf.name_scope('embed_tokens'):
+            embeddings = self._embedding_variable(
+                num_tokens, self.embedding_dims.tokens)
+            token_embedding = tf.nn.embedding_lookup(embeddings, tokens_with_GO)
+            self._activation_summary(token_embedding)
         with tf.variable_scope('decoder'):
             cell = self._rnn_cell()
             cell = tf.contrib.rnn.OutputProjectionWrapper(cell, num_tokens)
@@ -226,21 +224,33 @@ class Network:
 
     def loss_layer(self, logits, targets):
         # http://r2rt.com/recurrent-neural-networks-in-tensorflow-i.html
-        with tf.name_scope("loss", [logits, targets]):
+        with tf.name_scope('remove_GO'):
             slice_GO_token = tf.slice(
                 logits, begin=[0, 1, 0], size=[-1, -1, -1])
             num_classes = tf.shape(slice_GO_token)[2]
+        with tf.name_scope('flatten_logits'):
             logits_flat = tf.reshape(slice_GO_token, [-1, num_classes])
             targets = tf.reshape(targets, [-1])
-            crossent = tf.nn.sparse_softmax_cross_entropy_with_logits(
+        with tf.name_scope('cross_entropy'):
+            cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(
                 labels=targets, logits=logits_flat)
-            total_loss = tf.reduce_mean(crossent)
-            return total_loss
+            total_loss = tf.reduce_mean(cross_entropy)
+        tf.summary.scalar('cross_entropy', total_loss)
+
+        with tf.name_scope('accuracy'):
+            with tf.name_scope('correct_prediction'):
+                correct_prediction = tf.equal(
+                    tf.argmax(logits_flat, 1), tf.cast(targets, tf.int64))
+            with tf.name_scope('accuracy'):
+                self.accuracy = tf.reduce_mean(
+                    tf.cast(correct_prediction, tf.float32))
+        tf.summary.scalar('accuracy', self.accuracy)
+        return total_loss
 
     def train_layer(self, loss):
-        with tf.name_scope("train", [loss]):
+        with tf.name_scope('train'):
             train_step = tf.train.AdagradOptimizer(
-                self.learning_rate).minimize(loss)
+                self.learning_rate).minimize(loss, global_step=self.global_step)
             return train_step
 
     def _rnn_cell(self):
@@ -263,10 +273,4 @@ def _dummy():
 if __name__ == '__main__':
     network = Network(batch_size=100, validation_size=500, test_size=500)
     network.build()
-    sess = tf.InteractiveSession()
-    tf.global_variables_initializer().run()
-    for n in range(10):
-        print("Round {}".format(n))
-        feature_batch, token_batch = network.data.train.next_batch(100)
-        sess.run(network.rnn, feed_dict={network.input_pdfs: feature_batch,
-                                         network.tokens: token_batch})
+    network.train()
